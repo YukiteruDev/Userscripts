922ea0ebec6cd09771cd5b718a2d0d1b05b00638
Yukiteru <laurenceyoungdev@gmail.com>
2024-03-05 14:16:57 +0800

    Add reddit auto dark mode

---
+++ RedditAutoDarkMode.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        Reddit Auto Dark Mode
+// @namespace   Violentmonkey Scripts
+// @match       https://www.reddit.com/*
+// @grant       none
+// @version     1.0
+// @author      Yukiteru
+// @description Change Reddit's theme based on your system theme
+// ==/UserScript==
+
+function getToggleSwitch() {
+  return document.querySelector('faceplate-switch-input');
+}
+
+function isDarkMode() {
+  return getToggleSwitch().getAttribute('aria-checked') === 'true';
+}
+
+function toggleTheme(isDark) {
+  if (isDark !== isDarkMode()) getToggleSwitch().click(); // only toggles when the reddit theme and system theme does not match
+}
+
+const darkMedia = window.matchMedia('(prefers-color-scheme: dark)');
+setTimeout(() => toggleTheme(darkMedia.matches), 300)
+
+darkMedia.addEventListener('change', e => {
+  toggleTheme(e.matches); // toggle theme when system theme is changed
+});
